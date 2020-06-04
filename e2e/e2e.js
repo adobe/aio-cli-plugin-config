5cebdafd6c6185dcd012b94c81945073fd4b54b0
Shazron Abdullah <shazron@gmail.com>
2020-06-04 18:58:22 +0800

    fix: e2e tests

--- e2e/e2e.js
+++ e2e/e2e.js
@@ -10,23 +10,45 @@ governing permissions and limitations under the License.
 */
 
 const execa = require('execa')
-const chalk = require('chalk').default
+const chalk = require('chalk')
 const fs = require('fs')
+
+process.env.DEBUG = 'aio-cli-plugin-config*'
+// set global config file
+const CONFIG_FILE = '.e2e-aio-cli-plugin-config'
+process.env.AIO_CONFIG_FILE = CONFIG_FILE
+
+beforeEach(async () => {
+  fs.writeFileSync(CONFIG_FILE, '')
+})
+afterAll(async () => {
+  fs.unlinkSync(CONFIG_FILE)
+})
 
 test('config create test', async () => {
   const packagejson = JSON.parse(fs.readFileSync('package.json').toString())
   const name = `${packagejson.name}`
   console.log(chalk.blue(`> e2e tests for ${chalk.bold(name)}`))
 
+  let result
+
   console.log(chalk.bold('    - set a new config'))
   expect(() => { execa.sync('./bin/run', ['config:set', 'test_key', 'a value'], { stderr: 'inherit' }) }).not.toThrow()
 
+  console.log(chalk.bold('    - get a config (after it is set)'))
+  result = execa.sync('./bin/run', ['config:get', 'test_key'], { stderr: 'inherit' })
+  expect(result.stdout).toEqual('a value')
+
   console.log(chalk.bold('    - list config'))
-  const listResult = execa.sync('./bin/run', ['config:list'], { stderr: 'inherit' })
-  expect(listResult.output[1]).toEqual(expect.stringContaining('abc: "another value"'))
+  result = execa.sync('./bin/run', ['config:list'], { stderr: 'inherit' })
+  expect(result.stdout).toEqual(expect.stringContaining('test_key: "a value"'))
 
   console.log(chalk.bold('    - delete the test config'))
   expect(() => { execa.sync('./bin/run', ['config:delete', 'test_key'], { stderr: 'inherit' }) }).not.toThrow()
+
+  console.log(chalk.bold('    - get a config (after it is deleted)'))
+  result = execa.sync('./bin/run', ['config:get', 'test_key'], { stderr: 'inherit' })
+  expect(result.stdout).toEqual('')
 
   console.log(chalk.green(`    - done for ${chalk.bold(name)}`))
 })
