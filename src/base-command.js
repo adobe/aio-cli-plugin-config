2277767aecf091e0bb9e87861eb597e63d7c1783
Shazron Abdullah <36107+shazron@users.noreply.github.com>
2023-08-08 21:12:35 +0800

    fix eslint peer-dep issues, update js-yaml code

--- src/base-command.js
+++ src/base-command.js
@@ -35,7 +35,7 @@ class BaseCommand extends Command {
       if (format === 'json') {
         this.log(JSON.stringify(obj))
       } else if (format === 'yaml') {
-        this.log(yaml.safeDump(obj, { sortKeys: true, lineWidth: 1024, noCompatMode: true }))
+        this.log(yaml.dump(obj, { sortKeys: true, lineWidth: 1024, noCompatMode: true }))
       } else {
         if (typeof obj !== 'object') {
           this.log(obj)
